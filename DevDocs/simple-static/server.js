8772d3b20743863d9f64dfbf0576ff0cce8d1b31
Aviad Kim <aviadkim@gmail.com>
2025-04-18 00:21:28 +0300

    Add proper UI with Font Awesome icons and fix character encoding issues

--- DevDocs/simple-static/server.js
+++ DevDocs/simple-static/server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const PORT = process.env.PORT || 3002;
 
 const server = http.createServer((req, res) => {
-  // Read the index.html file
-  fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+  // Read the index-proper.html file
+  fs.readFile(path.join(__dirname, 'index-proper.html'), (err, content) => {
     if (err) {
       res.writeHead(500);
-      res.end('Error loading index.html');
+      res.end('Error loading index-proper.html');
       return;
     }
-    
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(content);
   });
