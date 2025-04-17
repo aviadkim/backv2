ae7d29ed2466a4512a1ab718ce0a4822a904cca2
Aviad Kim <aviadkim@gmail.com>
2025-04-17 23:57:47 +0300

    Use original UI with proper Font Awesome icons and fix character encoding issues

--- DevDocs/frontend/components/FontLoader.js
+++ DevDocs/frontend/components/FontLoader.js
@@ -14,13 +14,23 @@ const FontLoader = () => {
       />
       <link
         rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
         crossOrigin="anonymous"
         referrerPolicy="no-referrer"
       />
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <style jsx global>{`
+        @font-face {
+          font-family: 'Font Awesome 5 Free';
+          font-style: normal;
+          font-weight: 900;
+          font-display: block;
+          src: url(https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/webfonts/fa-solid-900.woff2) format('woff2'),
+               url(https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/webfonts/fa-solid-900.woff) format('woff');
+        }
+      `}</style>
     </Head>
   );
 };
